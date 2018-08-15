c46a774c2401e81d2c52dbfe21df45c1f2eb73a0
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-15 13:56:22 +0200

    something

--- AssetTest/Asset.cs
+++ AssetTest/Asset.cs
@@ -1,4 +1,53 @@
 ﻿class Asset
 {
+    private string assetName;
+    private decimal assetValue;
+    private decimal depricationRate;
 
+    public Asset(string assetName, decimal assetValue, decimal depticationRate)
+    {
+        AssetName = assetName;
+        AssetValue = assetValue;
+        DepricationRate = depricationRate;
+    }
+
+    public string AssetName
+    {
+        get
+        {
+            return assetName;
+        }
+        set
+        {
+            assetName = value;
+        }
+    }
+    public decimal AssetValue
+    {
+        get
+        {
+            return assetValue;
+        }
+        set
+        {
+            if (value > 0.0m)
+            {
+                assetValue = value;
+            }
+        }
+    }
+    public decimal DepricationRate
+    {
+        get
+        {
+            return DepricationRate;
+        }
+        set
+        {
+            if (value > 0.0m)
+            {
+                DepricationRate = value;
+            }
+        }
+    }
 }
