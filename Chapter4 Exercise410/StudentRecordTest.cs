84584d1532dbb7f26c636a9d2bcb91264aba8ff3
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-15 13:36:29 +0200

    Made studentRecordTest

--- Chapter4 Exercise410/StudentRecordTest.cs
+++ Chapter4 Exercise410/StudentRecordTest.cs
@@ -4,6 +4,38 @@ class StudentRecordTest
 {
     static void Main()
     {
-        Console.Write();
+        Student student1 = new Student("1", default, default, default, default);
+        Student student2 = new Student("2", default, default, default, default);
+
+        Console.Write("Enter Name for student1: ");
+        student1.StudentName = Console.ReadLine();
+        Console.Write("Enter Math marks: ");
+        student1.Math = decimal.Parse(Console.ReadLine());
+        Console.Write("Enter English marks: ");
+        student1.English = decimal.Parse(Console.ReadLine());
+        Console.Write("Enter Biology marks: ");
+        student1.Biology = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Press enter to continue");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Enter Name for student2: ");
+        student2.StudentName = Console.ReadLine();
+        Console.Write("Enter Math marks: ");
+        student2.Math = decimal.Parse(Console.ReadLine());
+        Console.Write("Enter English marks: ");
+        student2.English = decimal.Parse(Console.ReadLine());
+        Console.Write("Enter Biology marks: ");
+        student2.Biology = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Press enter to continue");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.Write($"Student1's aggregated score is: {student1.GetAggregate(student1.Math, student1.English, student1.Biology)}");
+        Console.Write($"\nStudent1's score percentage is: {student1.GetPercentage(student1.Math, student1.English, student1.Biology)}");
+
+        Console.ReadLine();
     }
 }
