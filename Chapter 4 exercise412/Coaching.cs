6fe688432a5ad6cde859b0991a1791fb43a62720
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-16 09:24:29 +0200

    finished coaching class

--- Chapter 4 exercise412/Coaching.cs
+++ Chapter 4 exercise412/Coaching.cs
@@ -1,4 +1,66 @@
 ﻿class Coaching
 {
+    private string coachingType;
+    private int players;
+    private string classTimings;
+    private decimal charges;
 
+    public Coaching(string coachingType, int players, string classTimings, decimal charges)
+    {
+        CoachingType = coachingType;
+        Players = players;
+        ClassTimings = classTimings;
+        Charges = charges;
+    }
+
+    public string CoachingType
+    {
+        get
+        {
+            return coachingType;
+        }
+        set
+        {
+            coachingType = value;
+        }
+    }
+
+    public int Players
+    {
+        get
+        {
+            return players;
+        }
+        set
+        {
+            players = value;
+        }
+    }
+    public string ClassTimings
+    {
+        get
+        {
+            return classTimings;
+        }
+        set
+        {
+            classTimings = value;
+        }
+    }
+    public decimal Charges
+    {
+        get
+        {
+            return charges;
+        }
+        set
+        {
+            charges = value;
+        }
+    }
+
+    public string DisplayDetails()
+    {
+        return $"{CoachingType} \t {Players} \t {ClassTimings} \t {Charges}";
+    }
 }
